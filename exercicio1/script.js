0f940a90e74438319a8b9c1f8b05408e42affbe6
FernandaImai <fernanda.imai@gmail.com>
2023-02-06 17:16:40 -0300

    exercicios-operadores git commit -m exercicios-operadores
    git commit -m exercicios-operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let numero = prompt('Insira um número par:')
+let divisao = Number(numero %2)
+console.log(`Resto da divisão do número por 2: ${divisao}`)
+
+// resultado sempre igual a zero
+// resultado sempre igual a 1
